49f52575cb223d4ffdfd88aad4286b97df6b92ab
Juanmillan8 <jmilcen0212@iescarrillo.es>
2025-06-08 13:56:06 +0200

    El administrador de un grupo debe poder expulsar a un miembro del mismo, incluso si dicho miembro también tiene rol de administrador del grupo Un administrador debe ser capaz de asignar como administrador a otro miembro del grupo, otorgándole así las mismas funciones

--- src/app/components/group/group-view/group-view.component.ts
+++ src/app/components/group/group-view/group-view.component.ts
@@ -11,11 +11,12 @@ import { AuthService } from '../../../services/auth.service';
 import { UserEvent } from '../../../models/userevent.model';
 import { UserEventService } from '../../../services/userevent.service';
 import { FormsModule } from '@angular/forms';
+import { UserGroup } from '../../../models/usergroup.model';
 
 @Component({
   selector: 'app-group-view',
   standalone: true,
-  imports: [CommonModule, EventListComponent,FormsModule],
+  imports: [CommonModule, EventListComponent, FormsModule],
   templateUrl: './group-view.component.html',
   styleUrl: './group-view.component.css'
 })
@@ -27,8 +28,12 @@ export class GroupViewComponent implements OnInit {
   admins: Member[] | null = null;
   authMember: Member | null = null;
   filterTermMembers: string = '';
+  usersGroup: UserGroup[] = [];
+  toastMessage: string | null = null;
+  toastType: 'success' | 'danger' | 'warning' | 'info' = 'success';
+  toastTimeout: any = null;
 
-  constructor(private route: ActivatedRoute, private memberService: MemberService, private groupService: GroupService, private authService:AuthService) { }
+  constructor(private route: ActivatedRoute, private memberService: MemberService, private groupService: GroupService, private authService: AuthService, private userGroupService: UserGroupService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -40,51 +45,109 @@ export class GroupViewComponent implements OnInit {
         this.groupService.getGroupById(this.groupId).subscribe((group: Group) => {
           this.group = group;
         })
-      }
-      if (this.groupId) {
         this.memberService.getMembersByGroup(this.groupId).subscribe((members: Member[]) => {
           this.members = members;
         });
         this.memberService.getAdminByGroup(this.groupId).subscribe((admins: Member[]) => {
           this.admins = admins;
         });
-      }
-          this.authService.getUserDataAuth().subscribe(({ user, member }) => {
-            this.authMember = member;
-           
-          });
+        this.userGroupService.getByGroup(this.groupId).subscribe((usersGroup: UserGroup[]) => {
+          this.usersGroup = usersGroup;
+        })
 
+      }
+
+      this.authService.getUserDataAuth().subscribe(({ user, member }) => {
+        this.authMember = member;
+
+      });
     });
   }
 
   isAdmin(userId: string): boolean {
-  return this.admins?.some(admin => admin.id === userId) ?? false;
+    return this.admins?.some(admin => admin.id === userId) ?? false;
   }
 
-  isAuthUserAdmin(){
-    if(this.authMember){
+  isAuthUserAdmin() {
+    if (this.authMember) {
       return this.isAdmin(this.authMember.userAccountId);
-    }else{
+    } else {
       return false;
     }
   }
-get filteredMembers(): Member[] {
-  const term = this.filterTermMembers.trim().toLowerCase();
-  if (!this.members) {
-    return [];
+  makeAdmin(user: Member) {
+    let findUg = this.usersGroup.find((ug: UserGroup) => { return ug.groupId == this.groupId && ug.userId == user.id });
+    if (findUg) {
+      findUg.admin = true;
+      this.userGroupService.saveUserGroup(findUg).then(()=>{
+          this.showToast(`${user.name} is now an admin.`, 'success');
+      });
+    }
+  }
+  removeAdmin(user: Member) {
+    let findUg = this.usersGroup.find((ug: UserGroup) => { return ug.groupId == this.groupId && ug.userId == user.id });
+    let numberOfAdmins = this.usersGroup.filter((ug: UserGroup) => { return ug.groupId == this.groupId && ug.admin == true });
+
+
+    if (findUg && numberOfAdmins.length > 1) {
+      findUg.admin = false;
+      this.userGroupService.saveUserGroup(findUg).then(()=>{
+          this.showToast(`${user.name}  has been removed as an administrator.`, 'success');
+      });;
+    } else if (numberOfAdmins.length == 1) {
+        this.showToast(`There must be at least one administrator in the group.`, 'danger');
+    }
   }
 
-  return this.members.filter(m => {
-    // concatenamos los campos que queremos buscar
-    const haystack = [
-      m.name,
-      m.surname,
-      m.username
-    ]
-      .join(' ')
-      .toLowerCase();
+  removeUser(user: Member) {
+    let findUg = this.usersGroup.find((ug: UserGroup) => { return ug.groupId == this.groupId && ug.userId == user.id });
+    if (findUg) {
+      findUg.admin = true;
+      this.userGroupService.deleteUserGroup(findUg.id);
+      this.showToast(`There must be at least one administrator in the group.`, 'danger');
 
-    return haystack.includes(term);
-  });
+    }
+  }
+
+  get filteredMembers(): Member[] {
+    const term = this.filterTermMembers.trim().toLowerCase();
+    if (!this.members) {
+      return [];
+    }
+
+    return this.members.filter(m => {
+      // concatenamos los campos que queremos buscar
+      const haystack = [
+        m.name,
+        m.surname,
+        m.username
+      ]
+        .join(' ')
+        .toLowerCase();
+
+      return haystack.includes(term);
+    });
+  }
+  
+showToast(message: string, type: 'success' | 'danger' | 'warning' | 'info' = 'info', duration: number = 3000): void {
+  this.toastMessage = message;
+  this.toastType = type;
+
+  // Limpiar timeout anterior si existe
+  if (this.toastTimeout) {
+    clearTimeout(this.toastTimeout);
+  }
+
+  // Autocierre tras x milisegundos
+  this.toastTimeout = setTimeout(() => {
+    this.toastMessage = null;
+  }, duration);
+}
+
+closeToast(): void {
+  this.toastMessage = null;
+  if (this.toastTimeout) {
+    clearTimeout(this.toastTimeout);
+  }
 }
 }
