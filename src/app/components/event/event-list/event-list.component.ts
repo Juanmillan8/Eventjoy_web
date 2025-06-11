477287431aa66eeb48edf22a9240a7267d9080fc
Juanmillan8 <jmilcen0212@iescarrillo.es>
2025-06-11 21:28:58 +0200

    Corrección de errores y mejoras.

--- src/app/components/event/event-list/event-list.component.ts
+++ src/app/components/event/event-list/event-list.component.ts
@@ -113,7 +113,7 @@ export class EventListComponent implements OnInit {
   }
   joinEvent(eventId: string) {
     if (this.userId) {
-      let userEvent = new UserEvent("-1", this.userId, eventId, false);
+      let userEvent = new UserEvent("-1", this.userId, eventId);
       this.userEventService.createUserEvent(userEvent);
     }
   }
