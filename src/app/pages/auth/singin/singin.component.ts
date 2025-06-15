4ddb8f6efffe5e697ee96cc0c8a8197b3d2d0536
Juanmillan8 <jmilcen0212@iescarrillo.es>
2025-06-15 20:18:12 +0200

    Correccione: añadidos todos los campos al registro para evitar futuros errores.

--- src/app/pages/auth/singin/singin.component.ts
+++ src/app/pages/auth/singin/singin.component.ts
@@ -10,6 +10,7 @@ import { UserCredential } from '@angular/fire/auth';
 import { MemberService } from '../../../services/member.service';
 import { Member } from '../../../models/member.model';
 import { firstValueFrom } from 'rxjs';
+import { MemberValidation } from '../../../validations/member.validation';
 
 @Component({
   selector: 'app-singin',
@@ -24,27 +25,35 @@ export class SinginComponent {
   error: String | null = null;
   constructor(formBuilder: FormBuilder, private authService: AuthService, private router: Router, private memberService: MemberService) {
     this.registerForm = formBuilder.group({
-      'name': ['', [Validators.required]],
-      'surname': ['', [Validators.required]],
-      'username': ['', [Validators.required]],
-      'email': ['', [Validators.email]],
-      'password': ['', [Validators.required]]
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required,Validators.email]],
+      dni: ['', Validators.required],
+      phone: ['', Validators.required],
+      birthdate: ['', [Validators.required, MemberValidation.minimumAge(12)]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
     });
   }
   register() {
     this.error = ""
     if (this.registerForm.valid) {
-      let name = this.registerForm.get("name")?.value;
-      let surname = this.registerForm.get("surname")?.value;
-      let username = this.registerForm.get("username")?.value;
-      let email = this.registerForm.get("email")?.value;
-      let password = this.registerForm.get("password")?.value;
+    let name = this.registerForm.get("name")?.value;
+    let surname = this.registerForm.get("surname")?.value;
+    let username = this.registerForm.get("username")?.value;
+    let email = this.registerForm.get("email")?.value;
+
+    let dni = this.registerForm.get("dni")?.value;
+    let phone = this.registerForm.get("phone")?.value;
+    let birthdate = this.registerForm.get("birthdate")?.value;
+    let password = this.registerForm.get("password")?.value;
+
 
       //Comprobamos si existe un usuario con el mismo email
       firstValueFrom(this.memberService.getMemberByUsername(username)).then((memberFind: Member[]) => {
         if (memberFind && memberFind.length ==0) {
           this.authService.register({ email, password }).then((userCredential: UserCredential) => {
-            let member = new Member(userCredential.user.uid, userCredential.user.uid, name, surname, email, ROLE.MEMBER, "", "", "", "", "", 0, "EMAIL");
+            let member = new Member(userCredential.user.uid, userCredential.user.uid, name, surname, email, ROLE.MEMBER,"",dni,phone,birthdate,username, 0, "EMAIL");
             this.memberService.saveMember(member).then(() => {
               this.router.navigate(["/home"]);
             })
